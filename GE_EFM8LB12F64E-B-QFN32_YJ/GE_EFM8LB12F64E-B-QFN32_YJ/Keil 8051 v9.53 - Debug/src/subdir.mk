2134bbe58f0ec41e596d48800ae642ddab69595d
jyi <yi_jian27@163.com>
2018-01-08 10:00:53 +0800

    之前一版不是特别稳定，新做了一版。此版现存问题是：加入了UART1导致LED失败_SMBUS0读写封装好了接口并且不死机读0地址的204c正确_加入了I2C0未验证其正确性，但是SMBUS保持正常) SMBUS0频率为10.008KHZ, 仅存问题是I2C1读到0地址值错误为0xff,,SMBUS0写数据到一半会中断的问题已经通过多发几个I2C时序clock的方法解决。

---
+++ GE_EFM8LB12F64E-B-QFN32_YJ/GE_EFM8LB12F64E-B-QFN32_YJ/Keil 8051 v9.53 - Debug/src/subdir.mk
@@ -0,0 +1,46 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+A51_UPPER_SRCS += \
+../src/SILABS_STARTUP.A51 
+
+C_SRCS += \
+../src/GE_EFM8LB12F64E-B-QFN32_YJ_main.c \
+../src/InitDevice.c \
+../src/Interrupts.c \
+../src/uart_1.c 
+
+OBJS += \
+./src/GE_EFM8LB12F64E-B-QFN32_YJ_main.OBJ \
+./src/InitDevice.OBJ \
+./src/Interrupts.OBJ \
+./src/SILABS_STARTUP.OBJ \
+./src/uart_1.OBJ 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.OBJ: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Keil 8051 Compiler'
+	C51 "@$(patsubst %.OBJ,%.__i,$@)" || $(RC)
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/GE_EFM8LB12F64E-B-QFN32_YJ_main.OBJ: D:/WorkSpace/MyCode/GE_EFM8LB12F64E-B-QFN32_YJ/GE_EFM8LB12F64E-B-QFN32_YJ/inc/bsp.h D:/WorkSpace/MyCode/GE_EFM8LB12F64E-B-QFN32_YJ/GE_EFM8LB12F64E-B-QFN32_YJ/inc/uart_1.h D:/WorkSpace/MyCode/GE_EFM8LB12F64E-B-QFN32_YJ/GE_EFM8LB12F64E-B-QFN32_YJ/inc/InitDevice.h C:/SiliconLabs/SimplicityStudio/v4/developer/toolchains/keil_8051/9.53/INC/STDIO.H C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/EFM8LB1/inc/SI_EFM8LB1_Register_Enums.h D:/WorkSpace/MyCode/GE_EFM8LB12F64E-B-QFN32_YJ/GE_EFM8LB12F64E-B-QFN32_YJ/inc/EFM8LB1_SMBus_MasterMultibyte.h D:/WorkSpace/MyCode/GE_EFM8LB12F64E-B-QFN32_YJ/GE_EFM8LB12F64E-B-QFN32_YJ/inc/EFM8LB1_I2C_Slave.h C:/SiliconLabs/SimplicityStudio/v4/developer/toolchains/keil_8051/9.53/INC/STRING.H D:/WorkSpace/MyCode/GE_EFM8LB12F64E-B-QFN32_YJ/GE_EFM8LB12F64E-B-QFN32_YJ/inc/bsp_config.h D:/WorkSpace/MyCode/GE_EFM8LB12F64E-B-QFN32_YJ/GE_EFM8LB12F64E-B-QFN32_YJ/inc/efm8_config.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/EFM8LB1/inc/SI_EFM8LB1_Defs.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/shared/si8051Base/si_toolchain.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/shared/si8051Base/stdint.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/shared/si8051Base/stdbool.h
+
+src/InitDevice.OBJ: C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/EFM8LB1/inc/SI_EFM8LB1_Register_Enums.h D:/WorkSpace/MyCode/GE_EFM8LB12F64E-B-QFN32_YJ/GE_EFM8LB12F64E-B-QFN32_YJ/inc/InitDevice.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/EFM8LB1/inc/SI_EFM8LB1_Defs.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/shared/si8051Base/si_toolchain.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/shared/si8051Base/stdint.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/shared/si8051Base/stdbool.h
+
+src/Interrupts.OBJ: C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/EFM8LB1/inc/SI_EFM8LB1_Register_Enums.h D:/WorkSpace/MyCode/GE_EFM8LB12F64E-B-QFN32_YJ/GE_EFM8LB12F64E-B-QFN32_YJ/inc/EFM8LB1_SMBus_MasterMultibyte.h D:/WorkSpace/MyCode/GE_EFM8LB12F64E-B-QFN32_YJ/GE_EFM8LB12F64E-B-QFN32_YJ/inc/EFM8LB1_I2C_Slave.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/EFM8LB1/inc/SI_EFM8LB1_Defs.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/shared/si8051Base/si_toolchain.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/shared/si8051Base/stdint.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/shared/si8051Base/stdbool.h
+
+src/%.OBJ: ../src/%.A51
+	@echo 'Building file: $<'
+	@echo 'Invoking: Keil 8051 Assembler'
+	AX51 "@$(patsubst %.OBJ,%.__ia,$@)" || $(RC)
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/uart_1.OBJ: D:/WorkSpace/MyCode/GE_EFM8LB12F64E-B-QFN32_YJ/GE_EFM8LB12F64E-B-QFN32_YJ/inc/uart_1.h D:/WorkSpace/MyCode/GE_EFM8LB12F64E-B-QFN32_YJ/GE_EFM8LB12F64E-B-QFN32_YJ/inc/efm8_config.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/EFM8LB1/inc/SI_EFM8LB1_Register_Enums.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/EFM8LB1/inc/SI_EFM8LB1_Defs.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/shared/si8051Base/si_toolchain.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/shared/si8051Base/stdint.h C:/SiliconLabs/SimplicityStudio/v4/developer/sdks/8051/v4.1.1/Device/shared/si8051Base/stdbool.h
+
+
