1f7ee0aa11e93d51add2b75b6294fcd18789d8a6
RichardYJ <yi_jian27@163.com>
2018-07-16 12:07:53 +0800

    GE_EFM8LB12F64E-B-QFN32_YJ_V0（初始化）

--- GE_EFM8LB12F64E-B-QFN32_YJ/GE_EFM8LB12F64E-B-QFN32_YJ/inc/InitDevice.h
+++ GE_EFM8LB12F64E-B-QFN32_YJ/GE_EFM8LB12F64E-B-QFN32_YJ/inc/InitDevice.h
@@ -12,24 +12,10 @@
 // USER PROTOTYPES
 
 // $[Mode Transition Prototypes]
-extern void enter_BusFreeMode_from_RESET(void);
-extern void enter_DefaultMode_from_BusFreeMode(void);
+extern void enter_DefaultMode_from_RESET(void);
 // [Mode Transition Prototypes]$
 
 // $[Config(Per-Module Mode)Transition Prototypes]
-extern void CLOCK_0_enter_BusFreeMode_from_RESET(void);
-extern void WDT_0_enter_DefaultMode_from_BusFreeMode(void);
-extern void PORTS_0_enter_DefaultMode_from_BusFreeMode(void);
-extern void PORTS_1_enter_DefaultMode_from_BusFreeMode(void);
-extern void PBCFG_0_enter_DefaultMode_from_BusFreeMode(void);
-extern void TIMER01_0_enter_DefaultMode_from_BusFreeMode(void);
-extern void TIMER16_3_enter_DefaultMode_from_BusFreeMode(void);
-extern void TIMER16_4_enter_DefaultMode_from_BusFreeMode(void);
-extern void TIMER_SETUP_0_enter_DefaultMode_from_BusFreeMode(void);
-extern void SMBUS_0_enter_DefaultMode_from_BusFreeMode(void);
-extern void UARTE_1_enter_DefaultMode_from_BusFreeMode(void);
-extern void I2CSLAVE_0_enter_DefaultMode_from_BusFreeMode(void);
-extern void INTERRUPT_0_enter_DefaultMode_from_BusFreeMode(void);
 // [Config(Per-Module Mode)Transition Prototypes]$
 
 #endif
